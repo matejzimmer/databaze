610929a4ffa1e99628e45148acf9a3a7db92aee2
matejzimmer <maty.zimmer@gmail.com>
2023-04-01 15:33:48 +0200

    02- První úprava formuláře

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("index"));
+
 app.listen(port, () =>{
     console.log(`Server naslouchá na portu ${port}`);
 });
